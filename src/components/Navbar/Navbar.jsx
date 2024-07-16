ee7fe7f52007694e7a51557f047dd53ed57a91ed
MuhammadRifkyAkbar <2210631170088@student.unsika.ac.id>
2024-07-16 18:20:40 +0700

    add navbar#1

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+import Logo from "../../assets/logo.png";
 
+const Menu = [
+  {
+    id: 1,
+    title: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    title: "Best Seller",
+    link: "/#services",
+  },
+];
 const Navbar = () => {
   return (
     <div>
-      nav
+      <div className="container py-3 sm:py-0">
+        <div className="flex justify-between items-center">
+          <div>
+            <a href="" className="font-bold text-2xl sm:text-3xl flex gap-2">
+              <img src={Logo} alt="" className="w-10" />
+              Ajosh Books
+            </a>
+          </div>
+          <div>
+            <ul className="items-center gap-4 hidden sm:flex">
+              {Menu.map((item) => (
+                <li key={item.id}>
+                  <a href={item.link} className="inline-block py-4 px-4 hover:text-primary duration-200">{item.title} </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
